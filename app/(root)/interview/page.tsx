de2ce74570386bcba208289b1c68463f5b89c99e
Prasad Dinnapurkar <prasaddinnapurkar@gmail.com>
2025-12-26 11:12:36 +0530

    Implemented the interview genration UI

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import Agent from "@/components/Agent";
+
+const page = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+      <Agent userName="Prasad" userId="user1" type="generate"/>
+    </>
+  );
+};
+export default page;
